158ac0113a7138439fa6082c4873809746447b3a
angeloffpm <pian4845@colorado.edu>
2022-02-08 12:50:47 -0700

    Implemented RemoveTwos() on Main Branch

--- functions_to_implement.cpp
+++ functions_to_implement.cpp
@@ -1,3 +1,6 @@
+#include <vector>
+#include <string>
+
 /* String functions section */
 
 // Splits a single string on separator into a vector of strings
@@ -18,7 +21,12 @@ std::vector<int> MatchVectors(std::vector<int> a, std::vector<int> b);
 
 // divides an input integer by 2 until it is impossible to do so, then returns the final number.
 // (16 = 2 * 2 * 2 * 2 * 1 -> 1, 7 -> 7, 26 = 2 * 13 -> 13, 52 = 2 * 2 * 13 -> 13)
-int RemoveTwos(int original);
+int RemoveTwos(int original) {
+    while (original % 2 == 0) {
+        original == original / 2;
+    }
+    return original;
+}
 
 // takes a vector of integers and removes all elements evenly divisible by the passed in int
 std::vector<int> MultiplesFilter(std::vector<int>, int divides_by);
